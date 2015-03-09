2314e6084399953ad8264326dfbe1f1864f3c16c
Stephen Brimhall <stephen.brimhall@icloud.com>
2015-03-09 12:54:08 -0700

    Initial Commit

---
+++ FileIO/FileIO.h
@@ -0,0 +1,19 @@
+//
+//  FileIO.h
+//  FileIO
+//
+//  Created by Stephen Brimhall on 3/9/15.
+//  Copyright (c) 2015 Brimhall Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FileIO.
+FOUNDATION_EXPORT double FileIOVersionNumber;
+
+//! Project version string for FileIO.
+FOUNDATION_EXPORT const unsigned char FileIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileIO/PublicHeader.h>
+
+
